ef7479b8038656d80daa17c219beb33fb665c005
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2024-02-29 19:56:18 +0900

    feat: 비밀번호 로그인 테이블 추가

--- src/server/database/schemas/users.ts
+++ src/server/database/schemas/users.ts
@@ -1,8 +1,15 @@
-import { bigserial, pgTable, varchar } from 'drizzle-orm/pg-core'
+import { bigint, bigserial, boolean, pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core'
 
+/** 사용자 */
 export const users = pgTable('users', {
   id: bigserial('id', { mode: 'bigint' }).primaryKey(),
-  username: varchar('username', { length: 20 }).notNull().unique('users__uk--username'),
+  username: varchar('username', { length: 20 }).notNull().unique('user_accounts__uk--username'),
+  admin: boolean('admin').notNull().default(false),
 })
 
-export type Users = typeof users.$inferSelect
+/** 사용자 비밀번호 */
+export const userPassword = pgTable('user_password', {
+  id: bigint('user_id', { mode: 'bigint' }).notNull().references(() => users.id).primaryKey(),
+  encryptedPassword: text('encrypted_password').notNull(),
+  lastChangedAt: timestamp('last_changed_at', { mode: 'date', withTimezone: true }).notNull().defaultNow(),
+})
