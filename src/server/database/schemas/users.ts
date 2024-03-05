102438defbc301dda09950ec2854b4c0268acb36
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2024-03-06 06:40:44 +0900

    refactor: id 값을 number로 변경

--- src/server/database/schemas/users.ts
+++ src/server/database/schemas/users.ts
@@ -2,14 +2,14 @@ import { bigint, bigserial, boolean, pgTable, text, timestamp, varchar } from 'd
 
 /** 사용자 */
 export const users = pgTable('users', {
-  id: bigserial('id', { mode: 'bigint' }).primaryKey(),
+  id: bigserial('id', { mode: 'number' }).primaryKey(),
   username: varchar('username', { length: 20 }).notNull().unique('user_accounts__uk--username'),
   admin: boolean('admin').notNull().default(false),
 })
 
 /** 사용자 비밀번호 */
 export const userPassword = pgTable('user_password', {
-  id: bigint('user_id', { mode: 'bigint' }).notNull().references(() => users.id).primaryKey(),
+  id: bigint('user_id', { mode: 'number' }).notNull().references(() => users.id).primaryKey(),
   encryptedPassword: text('encrypted_password').notNull(),
   lastChangedAt: timestamp('last_changed_at', { mode: 'date', withTimezone: true }).notNull().defaultNow(),
 })
